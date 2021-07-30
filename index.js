cb74d7791756488ebea43991bf63e72a7560d8ac
arr-atiq <atiq.rh01@gmail.com>
2021-07-31 01:37:28 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const ObjectId = require('mongodb').ObjectId;
-const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config()
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) =>{
-    res.send("i am working")
-})
-app.listen(4000);
+const uri = `mongodb+srv://${process.env.DB_USER}:${DB_PASS}@cluster0.skqkk.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+
+client.connect((err) => {
+  const student = client.db("studentBook").collection("student");
+  const subject = client.db("studentBook").collection("subject");
+  
+});
+
+app.listen(process.env.PORT || port);
